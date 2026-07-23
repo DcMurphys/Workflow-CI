dcc80b0525fd3cabec96caf27ec91f0384959792
Dwi Cahaya <99345021+DcMurphys@users.noreply.github.com>
2026-07-23 16:57:41 +0800

    Add modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -39,6 +39,9 @@ logging.basicConfig(
 # ===== Konfigurasi awal MLFlow tracking URI =====
 if "MLFLOW_TRACKING_URI" in os.environ:
     mlflow.set_tracking_uri(os.environ["MLFLOW_TRACKING_URI"])
+if "MLFLOW_TRACKING_USERNAME" is os.environ or "MLFLOW_TRACKING_PASSWORD" in os.environ:
+    MLFLOW_TRACKING_USERNAME = os.environ["MLFLOW_TRACKING_USERNAME"]
+    MLFLOW_TRACKING_PASSWORD = os.environ["MLFLOW_TRACKING_PASSWORD"]
 
 # ===== FUNGSI HELPER TERKAIT UTILITY =====
 def get_input_example(X, n_rows=5):
@@ -104,6 +107,22 @@ def get_x_and_y(df, target_col):
 
 
 # ===== FUNGSI HELPER TERKAIT MLFLOW MANUAL LOGGING =====
+def setup_tracking(experiment_name, dagshub_repo_owner=None, dagshub_repo_name=None):
+    """
+    Setup MLflow dan Dagshub (apabila ada dan ingin menerapkan penilaian Advanced)
+    """
+    if dagshub_repo_owner and dagshub_repo_name:
+        import dagshub
+
+        dagshub.init(
+            repo_owner=dagshub_repo_owner,
+            repo_name=dagshub_repo_name,
+            mlflow=True
+        )
+    
+    mlflow.set_experiment(experiment_name)
+
+
 def fit_search(search, X_train, y_train):
     """
     Pencarian model dengan parameter terbaik berdasarkan hasil cross validation scoring
@@ -518,7 +537,14 @@ def run_tracked_experiment(
             raise
 
 
-# ===== EKSEKUSI TRACKING EXPERIMENT TUNED MODEL DENGAN MLFLOW =====
+# ===== EKSEKUSI TRACKING EXPERIMENT TUNED MODEL DENGAN MLFLOW + DAGSHUB =====
+# Setup tracking experiment 
+setup_tracking(
+     experiment_name="gradient_boost_tuned",
+     dagshub_repo_name="customer-churn-prediction-system-workflow-ci",
+     dagshub_repo_owner=MLFLOW_TRACKING_USERNAME
+)
+
 # Memperoleh nilai random state untuk menjaga keacakan nilai pada setiap skenario
 RANDOM_STATE = 126 
 
@@ -575,7 +601,7 @@ search = RandomizedSearchCV(
      random_state=RANDOM_STATE
 )
 
-# Melacak eksperimen model hasil tuning dengan MLflow manual log 
+# Melacak eksperimen model hasil tuning dengan MLflow manual log + Dagshub
 result = run_tracked_experiment(
     search=search,
     X_train=X_train,
